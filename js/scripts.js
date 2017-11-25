07a061496daf48f4747021fd8d298748925e0a91
Artur Rymsza <artur.rymsza@gmail.com>
2017-11-25 11:53:30 +0100

    Add all initial files

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// scripts.js
+
+var name = prompt('Enter your name');
+// alert('Hello, ' + name);
+console.log('Hello, ' + name);
+
+/* This was my first fragment of
+Javascript code */
+
+// I find it very friendly
